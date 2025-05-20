a131665d3707960a8f4a6e318d4d0046e568b7c9
Elias-m-S <esehmsdorf68@gmail.com>
2025-05-20 19:54:59 +0200

    small Game to experience Raylib

--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
 #include "raylib.h"
 
 int main() {
-    // Fenster initialisieren (Breite, Höhe, Titel)
-    InitWindow(800, 600, "Pac-Man Fenster");
-
-    // Framerate setzen (optional)
+    // Fenster erstellen
+    InitWindow(800, 600, "Raylib Testgame");
     SetTargetFPS(60);
 
-    // Haupt-Game-Loop
+    // Spieler-Position
+    Vector2 playerPos = { 400, 300 };
+    float speed = 5.0f;
+
+    // Game Loop
     while (!WindowShouldClose()) {
+        // Eingabe
+        if (IsKeyDown(KEY_RIGHT)) playerPos.x += speed;
+        if (IsKeyDown(KEY_LEFT))  playerPos.x -= speed;
+        if (IsKeyDown(KEY_UP))    playerPos.y -= speed;
+        if (IsKeyDown(KEY_DOWN))  playerPos.y += speed;
+
+        // Zeichnen
         BeginDrawing();
         ClearBackground(RAYWHITE);
-
-        DrawText("Hallo Till", 350, 280, 20, DARKGRAY);
-
+        DrawText("Benutze Pfeiltasten!", 10, 10, 20, DARKGRAY);
+        DrawRectangleV(playerPos, {50, 50}, BLUE);
         EndDrawing();
     }
 
-    // Fenster und Ressourcen freigeben
+    // Aufräumen
     CloseWindow();
-
     return 0;
 }
