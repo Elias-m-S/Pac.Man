5826d632e6760b9507609479c46125cbe1bd019f
Elias-m-S <esehmsdorf68@gmail.com>
2025-05-20 19:52:27 +0200

    Endlich mit Cmake für raylib eingerichtet.

---
+++ build/CMakeFiles/pacman.dir/main.cpp.obj.d
@@ -0,0 +1,10 @@
+CMakeFiles/pacman.dir/main.cpp.obj: C:\Dev\Projects\Pac.Man\main.cpp \
+ C:/raylib/include/raylib.h \
+ C:/Dev/mingw64/lib/gcc/x86_64-w64-mingw32/14.2.0/include/stdarg.h \
+ C:/Dev/mingw64/x86_64-w64-mingw32/include/stdarg.h \
+ C:/Dev/mingw64/x86_64-w64-mingw32/include/_mingw_stdarg.h \
+ C:/Dev/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Dev/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Dev/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Dev/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Dev/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h
