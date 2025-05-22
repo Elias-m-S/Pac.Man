e714fa912cdea3010fbbbbc5d34554730774da91
Till W. <till.winter13@gmail.com>
2025-05-22 13:56:58 +0200

    Ordnerstruktur gebildet

--- build/CMakeFiles/pacman.dir/DependInfo.cmake
+++ build/CMakeFiles/pacman.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Dev/Projects/Pac.Man/main.cpp" "CMakeFiles/pacman.dir/main.cpp.obj" "gcc" "CMakeFiles/pacman.dir/main.cpp.obj.d"
+  "D:/Programmieren/Projekte/Pac.Man/main.cpp" "CMakeFiles/pacman.dir/main.cpp.obj" "gcc" "CMakeFiles/pacman.dir/main.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
