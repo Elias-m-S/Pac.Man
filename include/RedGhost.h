bdf4a94e382fd7abc8d9ed00ab6785739793dcd0
Till W. <till.winter13@gmail.com>
2025-06-09 11:37:32 +0200

    Proofing

--- include/RedGhost.h
+++ include/RedGhost.h
@@ -6,12 +6,12 @@ class RedGhost : public Ghostbase {
 public:
     RedGhost(const Map& map, int startX, int startY);
 
-    // Hier kannst du später den Bewegungsalgorithmus überschreiben
+    //Update um Bewegungsalgorythmus zu überschreiben
     void update(float lastTime, const Vector2& pacmanPos) override;
     Vector2 getTargetTile(const Vector2& pacmanPos) const override;
 
 private:
-    // spezifische Variablen für RedGhost (z. B. Zielverhalten)
+    // spezifische Variablen für RedGhost (z.B. Zielverhalten)
 
 
 };
